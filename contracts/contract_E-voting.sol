bede27031b9ba6cf2121e17afcaf14414083f7bd
Andikasndy <kentungj97@gmail.com>
2024-04-26 01:20:01 +0700

    v.1

---
+++ contracts/contract_E-voting.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/** 
+ * @title Ballot
+ * @dev Implements voting process along with vote delegation
+ */
+contract voting {
+
+    struct pemilih {
+        uint weight; // weight is accumulated by delegation
+        bool voted;  // if true, that person already voted
+        address delegate; // person delegated to
+        uint vote;   // index of the voted proposal
+    }
+
+    struct Proposal {
+        // If you can limit the length to a certain number of bytes, 
+        // always use one of bytes1 to bytes32 because they are much cheaper
+        string name;   // short name (up to 32 bytes)
+        uint voteCount; // number of accumulated votes
+    }
+
+    address public ketuapemilu;
+
+    mapping(address => pemilih) public voters;
+
+    Proposal[] public proposals;
+
+    
+    constructor(string[] memory proposalNames) {
+        ketuapemilu = msg.sender;
+        voters[ketuapemilu].weight = 1;
+
+        for (uint i = 0; i < proposalNames.length; i++) {
+            // 'Proposal({...})' creates a temporary
+            // Proposal object and 'proposals.push(...)'
+            // appends it to the end of 'proposals'.
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
+        }
+    }
+
+    /** 
+     * @dev Give 'voter' the right to vote on this ballot. May only be called by 'chairperson'.
+     * @param voter address of voter
+     */
+    function addresyangikutVote(address voter) public {
+        require(
+            msg.sender == ketuapemilu,
+            "hanya ketua pemilu yang dapat mengakses siapa yang dapat memilih"
+        );
+        require(
+            !voters[voter].voted,
+            "pemilih ini sudah memilih sebelumnya"
+        );
+        require(voters[voter].weight == 0);
+        voters[voter].weight = 1;
+    }
+
+  
+
+    /**
+     * @dev Give your vote (including votes delegated to you) to proposal 'proposals[proposal].name'.
+     * @param proposal index of proposal in the proposals array
+     */
+    function vote(uint proposal) public {
+        pemilih storage sender = voters[msg.sender];
+        require(sender.weight != 0, "tidak memiliki hak untuk memilih");
+        require(!sender.voted, "sudah memilih");
+        sender.voted = true;
+        sender.vote = proposal;
+
+        // If 'proposal' is out of the range of the array,
+        // this will throw automatically and revert all
+        // changes.
+        proposals[proposal].voteCount += sender.weight;
+    }
+
+    /** 
+     * @dev Computes the winning proposal taking all previous votes into account.
+     * @return winningProposal_ index of winning proposal in the proposals array
+     */
+    function winningProposal() public view
+            returns (uint winningProposal_)
+    {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    /** 
+     * @dev Calls winningProposal() function to get the index of the winner contained in the proposals array and then
+     * @return winnerName_ the name of the winner
+     */
+    function winnerName() public view  returns (string memory winnerName_)
+    {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+}
